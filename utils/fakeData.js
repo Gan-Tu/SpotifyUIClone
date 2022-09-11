0afb9c784bcbf837a3b11e3e9f2b53b8a5e75f1b
Gan Tu <github@tugan.dev>
2022-09-11 19:07:13 -0400

    fix typo

--- utils/fakeData.js
+++ utils/fakeData.js
@@ -8,7 +8,7 @@ export const getPlaylists = () => {
     "Lofi Fruits Music lofi hip",
     "My Shazam Tracks",
     "Pop Mix",
-    "Sleep Lofi Beats 2022 (Anime,)",
+    "Sleep Lofi Beats 2022 (Anime)",
     "TiE",
     "Today's Top Hits",
     "Your Top Songs 2021",
