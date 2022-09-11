108c5a431f24168d2bb525b92df2693a55016a71
Gan Tu <github@tugan.dev>
2022-09-11 18:29:06 -0400

    add solid icon sidebar menu

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import React from "react";
-import { HomeIcon, StackIcon, SearchIcon } from "./Icons";
+import { HomeIcon, StackIcon, SearchIcon, PlusIcon, HeartIcon } from "./Icons";
 
 function MenuItem({ Icon, Label, isActive }) {
   return (
@@ -22,7 +22,23 @@ function SideBar() {
       <div className="text-white flex flex-col space-y-4 p-4">
         <MenuItem Icon={HomeIcon} Label="Home" isActive />
         <MenuItem Icon={SearchIcon} Label="Search" />
-        <MenuItem Icon={StackIcon} Label="Your Library" />
+        <MenuItem
+          Icon={() => <StackIcon className="w-6 h-6 -rotate-90" />}
+          Label="Your Library"
+        />
+        <br />
+        <MenuItem
+          Icon={() => (
+            <PlusIcon className="w-6 h-6 bg-gray-400 max-w-fit text-gray-900 p-1 rounded-sm" />
+          )}
+          Label="Create Playlist"
+        />
+        <MenuItem
+          Icon={() => (
+            <HeartIcon className="w-6 h-6 bg-gradient-to-br from-indigo-500 via-indigo-600 to-blue-300 fill-gray-400 max-w-fit text-gray-900 p-1 rounded-sm" />
+          )}
+          Label="Liked Songs"
+        />
       </div>
     </div>
   );
